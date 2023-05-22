f2e8aa24c4be5d6b0ec575506a0805f423085451
karindaskal <68071857+karindaskal@users.noreply.github.com>
2023-05-22 12:38:15 +0300

    add manegment

--- router/management.js
+++ router/management.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const {
     mostFavorableSong,
     mostFavorableArtist,
-    mostFavorableSongDecade
+    mostFavorableSongDecade,
+    longestSongs,
+    shortestSongs
 } = require("../controller/managementC")
 router.get("/songdecade", mostFavorableSongDecade)
 router.get("/artist", mostFavorableArtist)
 router.get("/song", mostFavorableSong)
+router.get("/longest", longestSongs)
+router.get("/shortest", shortestSongs)
 module.exports = router
